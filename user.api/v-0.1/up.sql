736d4fabaac8d59ea63d57ac75a93d626a1d92c6
ilya-skoropad <ilya.skoropad0@gmail.com>
2024-10-06 10:50:43 +0300

    create vtable roles

--- user.api/v-0.1/up.sql
+++ user.api/v-0.1/up.sql
@@ -1,14 +1,21 @@
 create schema user_api;
 
-CREATE
-EXTENSION "uuid-ossp";
+create extension "uuid-ossp";
 
-create table users
+create table "user"
 (
     id           uuid primary key     default uuid_generate_v4(),
     login        varchar(25) not null,
     nick         varchar(25) not null,
     password     varchar(50) not null,
+    role         int         not null,
     created_at   date        not null default now(),
     activated_at date
 );
+
+create table role (
+                      id serial primary key,
+                      name varchar(50) not null unique
+);
+
+alter table user_api.user add constraint user_fk_i foreign key (role) references role (id)
